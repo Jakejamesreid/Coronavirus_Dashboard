35538497a07f0a623ae868355b4452a0c47e7fa7
jakejamesreid <jakejamesreid@gmail.com>
2020-04-27 16:50:46 +0100

    Changed from XMLHttpRequest to fetch for better readability

--- apitest.js
+++ apitest.js
@@ -1,33 +1,15 @@
-//https://www.w3schools.com/xml/xml_http.asp
-function getData(URL, cb){
-    // xhr = XmlHttpRequest
-    var xhr = new XMLHttpRequest(); 
-
-    /* xhr Ready States
-    0 Unsent
-    1 Opened
-    2 Headers Received
-    3 Loading
-    4 Done
-    */
-    xhr.onreadystatechange = function() {
-
-        // When Data is received
-        if (this.readyState == 4 && this.status == 200){
-            // Change the inner HTML of the DIV to JSON response text
-             cb(JSON.parse(this.responseText));
-        }
-    };
-
-    xhr.open("GET", URL);
-    xhr.send();
-}
-
+// https://dev.to/johnpaulada/synchronous-fetch-with-asyncawait
 function writeToDocument(URL){
-    jsonObj = getData(URL, function(data){
-        jsonObj = data.features[0].attributes['ConfirmedCovidCases']
-        document.getElementById("result").innerHTML = jsonObj
+    const request = async () => {
+        const response = await fetch(URL);
+        const json = await response.json();
 
-    });
-    //document.getElementById("result").innerHTML = jsonObj
+        document.getElementById("result").innerHTML =json.features[0].attributes['ConfirmedCovidCases'];
+    }
+    
+    request();
+    
 }
+
+    
+    
