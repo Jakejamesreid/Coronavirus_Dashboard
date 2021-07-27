91510cbebd6e90bf4e2d9944c38393b28c6c206d
Jake Reid <jakejamesreid@gmail.com>
2021-07-27 20:01:30 +0100

    Adjust celery beat schedule

--- covid_dashboard/celery.py
+++ covid_dashboard/celery.py
@@ -21,7 +21,7 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 app.conf.beat_schedule = {
     'every=day': {
         'task': 'home.tasks.send_email',
-        'schedule': crontab(minute=0, hour=9),
+        'schedule': crontab(minute=18, hour=20),
         'args': ('shmaffle06@yahoo.co.uk',)
     }
 }
--- home/tasks.py
+++ home/tasks.py
@@ -1,24 +1,32 @@
-from celery import shared_task
+# Standard Library
 from time import sleep
+import requests
+import json
+
+# Third Party
+from celery import shared_task
+from celery.exceptions import SoftTimeLimitExceeded
 from django.http import HttpResponse
 from django.core.mail import send_mail
 from django.conf import settings
-import requests
-import json
 
 
 @shared_task
 def send_email(recipients):
-    result = json.loads(requests.get(
-        'https://services1.arcgis.com/eNO7HHeQ3rUcBllm/arcgis/rest/services/CovidStatisticsProfileHPSCIrelandOpenData/FeatureServer/0/query?where=1%3D1&outFields=*&returnGeometry=false&orderByFields=date desc&outSR=4326&f=json'
-    ).text)
-    dailyCases = result['features'][0]['attributes']['ConfirmedCovidCases']
-    print(f'There are {dailyCases} today, sent to: {recipients}')
-    
-    send_mail(
-        'Covid Cases', 
-        f'There are {dailyCases} today', 
-        settings.EMAIL_HOST_USER, 
-        [recipients],
-        fail_silently=False)
+    try:
+        result = json.loads(requests.get(
+            'https://services1.arcgis.com/eNO7HHeQ3rUcBllm/arcgis/rest/services/CovidStatisticsProfileHPSCIrelandOpenData/FeatureServer/0/query?where=1%3D1&outFields=*&returnGeometry=false&orderByFields=date desc&outSR=4326&f=json'
+        ).text)
+        dailyCases = result['features'][0]['attributes']['ConfirmedCovidCases']
+        print(f'There are {dailyCases} today, sent to: {recipients}')
+        
+        send_mail(
+            'Covid Cases', 
+            f'There are {dailyCases} today', 
+            settings.EMAIL_HOST_USER, 
+            [recipients],
+            fail_silently=False)
+    except SoftTimeLimitExceeded as e:
+        print(e)
+
     return None
